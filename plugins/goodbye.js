99bdf95fbc02dfa8ad632fe60819ed9598c7c230
CMD TØXÏC <promiseability845@gmail.com>
2026-07-05 00:28:07 +0000

    Add goodbye command for enabling/disabling feature

---
+++ plugins/goodbye.js
@@ -0,0 +1,30 @@
+const { loadSettings, saveSettings } = require("../lib/settings");
+
+module.exports={
+
+name:"goodbye",
+category:"group",
+
+async execute({sock,from,msg,args}){
+
+const settings=await loadSettings();
+
+if(!args[0])
+return sock.sendMessage(from,{
+text:`Usage
+
+.goodbye on
+.goodbye off`
+},{quoted:msg});
+
+settings.goodbye=args[0]=="on";
+
+await saveSettings(settings);
+
+sock.sendMessage(from,{
+text:`✅ Goodbye ${settings.goodbye?"Enabled":"Disabled"}`
+,{quoted:msg});
+
+}
+
+}
